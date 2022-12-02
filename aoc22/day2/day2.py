8f3e1ba54c71fbdd999266b02d1272941ed3476f
Charlie Taylor <peekoecode@gmail.com>
2022-12-01 21:47:00 -0800

    day 2!

---
+++ aoc22/day2/day2.py
@@ -0,0 +1,30 @@
+with open('input.txt') as file:
+  lines = [line.rstrip().lower() for line in file]
+
+match = {
+  'a': 'x',
+  'b': 'y',
+  'c': 'z'
+}
+
+values = {
+  'x': 1,
+  'y': 2,
+  'z': 3
+}
+
+points = 0
+
+for line in lines:
+  opp, me = line.split()
+
+  oppMap = match[opp]
+
+  if oppMap == me:
+    points += 3 + values[me]
+  elif values[me] == values[oppMap] + 1 or values[oppMap] == values[me] + 2:
+    points += 6 + values[me]
+  else:
+    points += values[me]
+
+print(points)
---
+++ aoc22/day2/day2_part2.py
@@ -0,0 +1,22 @@
+with open('input.txt') as file:
+  lines = [line.rstrip().lower() for line in file]
+
+values = {
+  'a': 1,
+  'b': 2,
+  'c': 3
+}
+
+points = 0
+
+for line in lines:
+  opp, end = line.split()
+
+  if end == 'x':
+    points += (3 if values[opp] == 1 else values[opp] - 1)
+  elif end == 'y':
+    points += 3 + values[opp]
+  else:
+    points += 6 + (1 if values[opp] == 3 else values[opp] + 1)
+
+print(points)
