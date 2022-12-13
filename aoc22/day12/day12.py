23ae188922e7ee35fa2541012e4fa1d437db0083
Charlie Taylor <peekoecode@gmail.com>
2022-12-12 19:22:47 -0800

    pain

--- aoc22/day12/day12.py
+++ aoc22/day12/day12.py
@@ -3,61 +3,59 @@ from pprint import pprint as pp
 with open('input.txt') as f:
   lines = [x.strip() for x in f.readlines()]
 
+class Node:
+  def __init__(self, pos, height) -> None:
+    self.pos = pos
+    self.height = height
+    self.path = []
+
 # S is start
 grid = []
-adj = {}
 
-S = ()
-E = ()
+S = Node((0,0), -1)
+E = Node((0,0), -1)
 
 for i, line in enumerate(lines):
   temp = []
   for j, x in enumerate(line):
     if x == 'E':
-      E = (i, j)
+      E = Node((i, j), 1)
       temp.append(26)
     elif x == 'S':
-      S = (i,j)
+      S = Node((i,j), 26)
       temp.append(1)
     else:
       temp.append(ord(x) - 96)
   grid.append(temp)
 
-# pp(grid)
-# print(S, E)
+pp(grid)
+print(S, E)
+# for dir in [[1,0],[-1,0],[0,1],[0,-1]]
 
-for row in range(len(grid)):
-  for col in range(len(grid[row])):
-    adj[(row, col)] = []
+def bfs(start):
+  queue: list[Node] = [start]
+  visited = set([start])
 
-    for dir in [[1,0],[-1,0],[0,1],[0,-1]]:
-      try:
-        if row + dir[0] < 0 or col + dir[1] < 0:
-          continue
-
-        next = grid[row + dir[0]][col + dir[1]]
-        if grid[row][col] - next in range(-1,2):
-          adj[(row, col)].append((row + dir[0], col + dir[1]))
-      except:
-        pass
+  while len(queue):
+    curr = queue.pop(0)
+    if curr.pos == E.pos:
+      print(f"DONE {len(curr.path)}")
+      return len(curr.path)
     
-# pp(adj)
+    for r, c in [[1,0],[-1,0],[0,1],[0,-1]]:
+      n = (curr.pos[0] + r, curr.pos[1] + c)
+      if n[0] < 0 or n[0] > len(grid[0]) or n[1] < 0 or n[1] > len(grid):
+        continue
 
-# visited = []
-queue = []
+      next = grid[n[0]][n[1]]
+      if next - curr.height > 1 or curr in visited:
+        continue
 
-def bfs(visited, graph, node):
-  visited.append(node)
-  queue.append(node)
-  
-  while queue:
-    s = queue.pop(0)
-    print(s)
-    if s == E:
-      print(f'DONE! {len(visited)}')
-    for n in graph[s]:
-      # if n not in visited:
-        visited.append(n)
-        queue.append(n)
+      node = Node(curr, next)
+      node.path = curr.path[:]
+      node.path.append(curr)
+      visited.append(curr)
+      queue.append(node)
+  return 1_000_000_000
 
-bfs([], adj, (0,0))
+print(bfs(S))
