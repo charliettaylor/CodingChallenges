0bfab5fa61ae97d983874ad1a9a9c280e8c052c1
Charlie Taylor <peekoecode@gmail.com>
2023-03-01 14:46:52 -0800

    n time no division :)

---
+++ leetcode/product-of-array-except-self.py
@@ -0,0 +1,15 @@
+# 238: Product of Array Except Self
+
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        result = [1] * len(nums)
+        last = 1
+
+        for i in range(1, len(nums)):
+            result[i] = result[i - 1] * nums[i - 1]
+
+        for i in range(len(nums))[::-1]:
+            result[i] *= last
+            last *= nums[i]
+
+        return result
