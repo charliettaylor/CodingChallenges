4e7ab2b7286f9621c31b9a20d1ce94c064d14dfa
Charlie Taylor <peekoecode@gmail.com>
2024-01-03 19:10:03 -0800

    idk protohackers stuff

--- protohackers/p3.py
+++ protohackers/p3.py
@@ -1,34 +1,35 @@
-from functools import lru_cache
 import json
 import asyncio
 from dataclasses import dataclass
-from operator import truediv
+
 
 @dataclass
 class User:
     username: str
     writer: asyncio.StreamWriter
 
+
 HOST = "0.0.0.0"
 PORT = 8888
 
 users: list[User] = []
 
+
 async def handle(r: asyncio.StreamReader, w: asyncio.StreamWriter) -> None:
     async def send(data: str) -> None:
         print(data)
-        w.write(data.encode('utf8'))
+        w.write(data.encode("utf8"))
         w.write(b"\n")
         await w.drain()
 
     async def broadcast(user: User, data: str) -> None:
         print(data)
         for user in [x for x in users if x != user]:
-            user.writer.write(data.encode('utf8'))
+            user.writer.write(data.encode("utf8"))
             user.writer.write(b"\n")
             await user.writer.drain()
 
-    await send('Welcome to budgetchat! What shall I call you?')
+    await send("Welcome to budgetchat! What shall I call you?")
     username = (await r.readline()).decode().strip()
     user = User(username, w)
 
@@ -36,10 +37,10 @@ async def handle(r: asyncio.StreamReader, w: asyncio.StreamWriter) -> None:
         await w.drain()
         w.close()
 
-    listedUsers = ','.join([x.username for x in users])
-    await send('* The room contains: ' + listedUsers)
+    listedUsers = ",".join([x.username for x in users])
+    await send("* The room contains: " + listedUsers)
     users.append(user)
-    await broadcast(user, f'* {username} has entered the room')
+    await broadcast(user, f"* {username} has entered the room")
 
     while True:
         try:
@@ -48,13 +49,13 @@ async def handle(r: asyncio.StreamReader, w: asyncio.StreamWriter) -> None:
             if not message:
                 break
 
-            await broadcast(user, f'[{username}] {message.decode().strip()}')
+            await broadcast(user, f"[{username}] {message.decode().strip()}")
         except Exception as e:
             print(e)
             break
 
     users.remove(user)
-    await broadcast(user, f'* {username} has left the room')
+    await broadcast(user, f"* {username} has left the room")
     w.close()
 
 
