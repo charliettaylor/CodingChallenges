db3c44716d8052e4016544e9d091d2a30f166fb3
Charlie Taylor <peekoecode@gmail.com>
2024-03-24 19:13:52 -0700

    classic

---
+++ leetcode/invertTree/main.go
@@ -0,0 +1,22 @@
+// 226. Invert Binary Tree
+
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	temp := root.Left
+	root.Left = root.Right
+	root.Right = temp
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	return root
+}
