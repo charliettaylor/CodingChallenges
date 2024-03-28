9168516364269d54fcf45d0d887bafeaf8df73d7
Charlie Taylor <peekoecode@gmail.com>
2024-03-27 21:57:20 -0700

    why did i struggle with this one tho

---
+++ leetcode/reverseLinkedList/main.go
@@ -0,0 +1,19 @@
+// 206. Reverse Linked List
+
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+	return prev
+}
