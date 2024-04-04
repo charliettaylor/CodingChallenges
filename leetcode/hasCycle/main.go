ffdfa410bfe47bfe0a51d008e125c9ed8bf61656
Charlie Taylor <peekoecode@gmail.com>
2024-04-03 21:21:08 -0700

    🐢 and 🐇

---
+++ leetcode/hasCycle/main.go
@@ -0,0 +1,23 @@
+// 141. Linked List Cycle
+
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false
+}
