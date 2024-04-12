5db3419698e1d431616c98586e2df9dde315289c
Charlie Taylor <peekoecode@gmail.com>
2024-04-11 18:59:26 -0700

    trees 🌲

---
+++ leetcode/isSubtree/main.go
@@ -0,0 +1,33 @@
+// 572. Subtree of Another Tree
+
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil || subRoot == nil {
+		return root == subRoot
+	}
+
+	if isSameTree(root, subRoot) {
+		return true
+	}
+
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil || q == nil {
+		return p == q
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
--- leetcode/sameTree/main.go
+++ leetcode/sameTree/main.go
@@ -28,3 +28,16 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 
 	return p.Val == q.Val && solve(p.Left, q.Left) && solve(p.Right, q.Right)
 }
+
+// cleaner not dumb solution
+func isSameTreeImproved(p *TreeNode, q *TreeNode) bool {
+    if p == nil || q == nil {
+        return p == q
+    }
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
